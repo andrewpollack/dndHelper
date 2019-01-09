29f049b3faa0075e44107ad2f938e643084a6f42
Andrew Pollack <andrewpkq@gmail.com>
2019-01-08 21:20:30 -0800

    Added a guessing feature for guessing which classes you are typing!  Helpful for those who love their 'pally' and 'barb's!

--- dndHelper.py
+++ dndHelper.py
@@ -2,6 +2,32 @@
 import random
 import time
 
+
+def smartGuess(u_input, option_list):
+    """
+    Naive approach to guessing what the user has input in case
+    they didn't enter an exact choice.  Done so by taking highest percentage
+    of matching characters between the user's input and an option within the 
+    list.
+    """
+    if u_input in option_list:
+        return u_input
+    best_match_percent = -100
+    best_match_item = ""
+    for curr_item in option_list:
+        curr_len = len(curr_item)
+        copy_item = curr_item
+        for curr_letter in u_input:
+            let_pos = copy_item.find(curr_letter)
+            if(let_pos != -1):
+                copy_item = copy_item[:let_pos] + copy_item[let_pos+1:]
+        curr_match_percent = (curr_len - len(copy_item)) / curr_len - (len(u_input) / curr_len)
+        if(curr_match_percent > best_match_percent):
+            best_match_percent = curr_match_percent
+            best_match_item = curr_item
+    return best_match_item
+
+
 def rollDice(digit, num_dice):
     """
     Simulates rolling {num_dice} number of {digit} dice.
@@ -37,8 +63,8 @@ def diceRollMenu():
         roll_tot = rollDice(int(u_digit), int(u_num_dice))
         print("\nIn total, you rolled: " + str(roll_tot) + "!") 
         # Flavor text
-        # time.sleep(1)
-        cont = input("\nWould you like to roll another set of dice?  Yes/No: ")
+        # time.sleep(1) 
+        cont = smartGuess(input("\nWould you like to roll another set of dice?  Yes/No: "), ["yes", "no"])
     print("\nReturning to main menu...\n\n\n")
 
 
@@ -70,27 +96,27 @@ def classInformationMenu():
     classes = list(class_dict.keys())
     classes.sort()
     # Undercase classes used for input checks
-    uclasses = [x.lower() for x in classes]
+    uclasses = classes
     classes = [x.title() for x in classes]
     cont = "yes"
-    while(cont.lower() == "yes"): 
-        u_class = input("\nWhat class would you like to learn about?\nAll\n" + "\n".join(classes) + "\n\nClass: ").lower()
-        while u_class not in uclasses and u_class != "all":
-            print("\nPlease enter a valid class.")
-            u_class = input("What class would you like to learn about?\n\nClass: ").lower()
-        # Print full information of all classes.
+    while(cont.lower() == "yes"):
+        u_class = smartGuess(input("\nWhat class would you like to learn about?\nAll\n" + "\n".join(classes) + "\n\nClass: ").lower(), uclasses)
+        # Print all information on classes.
         if(u_class == "all"):
             print("Here's a basic list of the classes and their information!")
             for curr_class in uclasses:
-                print("\n" + class_dict[curr_class][0])
-                for curr_info in class_dict[curr_class][1:]:
-                    print(" " + curr_info)
+                if(curr_class == "all"):
+                    pass
+                else:
+                    print("\n" + class_dict[curr_class][0])
+                    for curr_info in class_dict[curr_class][1:]:
+                        print(" " + curr_info)
         # Print full information of specified class.
         else:
             print("\n" + class_dict[u_class][0])
             for curr_info in class_dict[u_class][1:]:
                 print(" " + curr_info)
-        cont = input("\nWould you like to learn about another class?  Yes/No: ")
+        cont = smartGuess(input("\nWould you like to learn about another class?  Yes/No: "), ["yes", "no"])
     print("\nReturning to main menu...\n\n\n")
 
 
